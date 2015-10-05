66f5d59c706e2bd5e9212e84b01bec688b4fbe47
opuro <vaclav.janc@gmail.com>
2015-10-05 15:21:12 +0200

    Swap MongoDB for SQLite

--- sockets.js
+++ sockets.js
@@ -1,5 +1,10 @@
 //this file exports the custom socket.io functionality to app.js and is provided the io object by app.js
-module.exports = function(app,io){
+module.exports = function(io){
+  var sqlite3 = require('sqlite3').verbose();
+  var db = new sqlite3.Database('database/sqlite.db');
+  db.serialize(function() {
+    db.run("CREATE TABLE if not exists threads (info TEXT)");
+  });
   var clients = {}, rooms = {};
   io.on('connection', function(socket){
     console.log('user connected');
