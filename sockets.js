09d0ecac70670a9378e3b2e8fd8c32ebf7ea09e0
Václav Jančařík <vaclav.janc@gmail.com>
2015-10-06 09:36:28 +0200

    Add rooms to database

--- sockets.js
+++ sockets.js
@@ -3,14 +3,15 @@ module.exports = function(io){
   var sqlite3 = require('sqlite3').verbose();
   var db = new sqlite3.Database('database/sqlite.db');
   db.serialize(function() {
-    db.run("CREATE TABLE if not exists threads (info TEXT)");
+    db.run("CREATE TABLE if not exists threads (name TEXT)");
   });
   var clients = {}, rooms = {};
   io.on('connection', function(socket){
     console.log('user connected');
-    socket.on('joinRoom',function(roomId){
-      socket.join(roomId);
-      socket.room = roomId;
+    socket.on('joinRoom',function(roomName){
+      db.run("INSERT INTO threads ('name') VALUES ('"+roomName+"')");
+      socket.join(roomName);
+      socket.room = roomName;
     });
     socket.on('disconnect', function(){
       console.log('user disconnected');
