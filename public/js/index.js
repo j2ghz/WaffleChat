315440450a07fa0aa2e7a9e65aa4b2fd4b539e0c
Václav Jančařík <vaclav.janc@gmail.com>
2015-10-16 13:27:53 +0200

    Save messages to database

--- public/js/index.js
+++ public/js/index.js
@@ -1,24 +1,29 @@
 /* global io from another file, provided by index.jade */
 var socket = io();
 var m = $('#messageInput');
-
 $("button#createThread").click(function(){
 	socket.emit('createThread', prompt("Select thread"));
 });
 $('#messageForm').submit(function(){
 	if(m.val()!==""){
-		socket.emit('chatMessage', m.val());
+		socket.emit('message', m.val());
 		m.val('');
 	}
 	return false;
 });
-socket.on('chatMessage', function(msg){
+socket.on('message', function(msg){
 	$('#messages').append($('<li>').text(msg));
+});
+socket.on('printMessages',function(data){
+	$("#messages").text("");
+	data.forEach(function(entry){
+		$("#messages").append("<li>"+entry.content+"</li>");	
+	});
 });
 socket.on('printThreads',function(data){
 	$("#threads").text("");
 	data.forEach(function(entry){
-		$("#threads").append("<a href='#' class='joinThread' data-id='"+entry.id+"'>"+entry.name+"</a><br>");	
+		$("#threads").append("<li><a href='#' class='joinThread' data-id='"+entry.id+"'>"+entry.name+"</a></li>");	
 	});
 	$("a.joinThread").click(function(){
 		socket.emit('joinThread',$(this).data('id'));
