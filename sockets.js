95b72fdc51ab3ce461b62afbe640e90a6e306338
Václav Jančařík <vaclav.janc@gmail.com>
2015-10-16 13:27:53 +0200

    Save messages to database

--- sockets.js
+++ sockets.js
@@ -3,40 +3,37 @@ module.exports = function(io){
   var sqlite3 = require('sqlite3').verbose();
   var db = new sqlite3.Database('database/sqlite.db');
   var uuid = require('node-uuid');
-  db.serialize(function() {
-    db.run("CREATE TABLE if not exists threads (id TEXT, name TEXT)");
-  });
+  db.run("CREATE TABLE if not exists threads (id TEXT, name TEXT)");
+  db.run("CREATE TABLE if not exists messages (thread TEXT, sender TEXT, content TEXT)");
   var listOfThreads = ["swag"]; //load list of threads from database
   io.on('connection', function(socket){
     socket.emit('updateThreads',listOfThreads);
-    console.log('user connected');
+    console.log('user connected');  
     db.all("SELECT id,name FROM threads", function(err,rows){
        io.emit("printThreads",rows);
-    });
-    
+    });  
     socket.on('createThread',function(threadName){
-      db.run("INSERT INTO threads ('id','name') VALUES ('"+uuid.v4()+"','"+threadName+"')");
-      socket.leave(socket.thread);
-      socket.join(threadName);
-      socket.thread = threadName;
-      listOfThreads.push(threadName);
+      var threadId = uuid.v4();
+      db.run("INSERT INTO threads ('id','name') VALUES ('"+threadId+"','"+threadName+"')");
+      listOfThreads.push(socket.thread);
       db.all("SELECT id,name FROM threads", function(err,rows){
         io.emit("printThreads",rows);
       });
     });
-    
-    socket.on('joinThread',function(threadName){
+    socket.on('joinThread',function(threadId){
       socket.leave(socket.thread);
-      socket.join(threadName);
-      socket.thread = threadName;
-    });
-    
+      socket.join(threadId);
+      socket.thread = threadId;
+      db.all("SELECT thread,content FROM messages WHERE thread='"+threadId+"'",function(err,rows){
+        io.emit("printMessages",rows);
+      });
+    });    
     socket.on('disconnect', function(){
       console.log('user disconnected');
-    });
-    
-    socket.on('chatMessage', function(msg){
-      io.in(socket.thread).emit('chatMessage', msg);
+    });   
+    socket.on('message', function(msg){
+      io.in(socket.thread).emit('message', msg);
+      db.run("INSERT INTO messages ('thread','content') VALUES ('"+socket.thread+"','"+msg+"')");
     });
   });
 }
