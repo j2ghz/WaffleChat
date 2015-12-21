ff044bb48c1ddfa5fdfa2af4aeb08d6a5970df3d
opuro <vaclav.janc@gmail.com>
2015-12-21 11:29:03 +0100

    Rework joining threads

--- database/conn.js
+++ database/conn.js
@@ -1,2 +1,2 @@
 var sqlite3 = require('sqlite3').verbose();
-module.exports = new sqlite3.Database('database/sqlite.db');
+module.exports = new sqlite3.Database('./database/sqlite.db');
--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,11 @@
 /* global io from another file, provided by index.jade */
 var socket = io();
 var m = $('#messageInput');
+
 $("button#createThread").click(function(){
 	socket.emit('createThread', prompt("Select thread"));
 });
+
 $('#messageForm').submit(function(){
 	if(m.val()!==""){
 		socket.emit('message', m.val());
@@ -11,22 +13,29 @@ $('#messageForm').submit(function(){
 	}
 	return false;
 });
+
 socket.on('message', function(msg){
 	$('#messages').append($('<li>').text(msg));
 });
+
 socket.on('printMessages',function(data){
 	$("#messages").text("");
 	data.forEach(function(entry){
 		$("#messages").append("<li>"+entry.content+"</li>");	
 	});
 });
+
 socket.on('printThreads',function(data){
 	$("#threads").text("");
 	data.forEach(function(entry){
-		$("#threads").append("<li><a href='#' class='joinThread' data-id='"+entry.id+"'>"+entry.name+"</a></li>");	
+		$("#threads").append("<li><a href='"+entry.id+"'>"+entry.name+"</a></li>");	
 	});
-	$("a.joinThread").click(function(){
-		socket.emit('joinThread',$(this).data('id'));
-		$("span#currentThread").text(" - "+$(this).text());
+	$("#threads a").click(function(e){
+        e.preventDefault();
+		socket.emit('joinThread',$(this).attr('href'));
 	});
+});
+
+socket.on('threadJoined',function(data){
+    $("span#currentThread").text(data);
 });
