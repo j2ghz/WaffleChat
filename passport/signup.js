0b381f03ac25785297f57f723f776f80a2deb6d5
Václav Jančařík <vaclav.janc@gmail.com>
2016-01-02 21:26:23 +0100

    Errors on bad login or if signing up as existing user

--- passport/signup.js
+++ passport/signup.js
@@ -1,7 +1,16 @@
 var db = require('../database/conn');
 var crypto = require('crypto');
 var hashPassword = require('./hash');
-module.exports = function(username, password) {
-	var salt = crypto.randomBytes(16).toString("hex");
-	db.run("INSERT INTO users ('username', 'password', 'salt') VALUES (?, ?, ?)", username, hashPassword(password, salt), salt);
+module.exports = function(username, password, next) {
+    db.get("SELECT username FROM users WHERE username = ?", username, function(err, row) {
+        if (row !== undefined) {
+            var error = new Error('Username already taken');
+            error.status = 401;
+            next(error);
+        } else {
+          	var salt = crypto.randomBytes(16).toString("hex");
+            db.run("INSERT INTO users ('username', 'password', 'salt') VALUES (?, ?, ?)", username, hashPassword(password, salt), salt);
+            next();  
+        }
+    });
 }
