a003ce4eb263e504deae6994492c21779bf8c885
Václav Jančařík <vaclav.janc@gmail.com>
2016-01-03 19:33:37 +0100

    Serve stylesheets and js using bower

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
 //database
 var db = require('./database/conn');
