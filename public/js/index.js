8266f4226f5781cafbdaa378d5a2c1cf8ec280d6
Václav Jančařík <vaclav.janc@gmail.com>
2016-01-06 17:24:59 +0100

    Change thread window functions to jquery methods

--- public/js/index.js
+++ public/js/index.js
@@ -3,23 +3,24 @@
 var socket = io(), myUsername, myThreads = [], lastDate = [],
     $chatContainer = $('#chatContainer'), $threads = $('#threads'), $footer = $('footer'); //caching jquery objects  
 
-//Create Thread button functionality
-$('button#createThread').click(function() {
-    swal({
-        title:'Enter a name',
-        text:'You can enter any characters you want.',
-        type:'input',
-        showCancelButton:true,
-        closeOnConfirm:true    
-    }, function(inputValue) {
-        if (inputValue === false) return false;
-        if (inputValue === '') {
-            swal.showInputError("You need to write something!");
-            return false;
-        }
-        socket.emit('createThread', inputValue); 
-    });
-	
+//on document load
+$(document).ready(function() { 
+    $('button#createThread').click(function() {
+        swal({
+            title:'Enter a name',
+            text:'You can enter any characters you want.',
+            type:'input',
+            showCancelButton:true,
+            closeOnConfirm:true    
+        }, function(inputValue) {
+            if (inputValue === false) return false;
+            if (inputValue === '') {
+                swal.showInputError("You need to write something!");
+                return false;
+            }
+            socket.emit('createThread', inputValue); 
+        });	
+    });  
 });
 
 socket.on('setUsername', function(name) {
@@ -30,33 +31,41 @@ socket.on('setUsername', function(name) {
 socket.on('printThreads', function(threads) {
 	$threads.text('');
 	threads.forEach(function(thread) {
-		$threads.prepend(ThreadListItem(thread.id, thread.name, thread.creator, thread.lastActivity, thread.lastSender));	
+		$threads.prepend(
+            ThreadListItem(thread.id, thread.name, thread.creator, thread.lastActivity, thread.lastSender)
+        );	
         cacheThreadListElements(thread.id);
 	});    
 });
 
 socket.on('printThread', function(id, name, creator) {
-    $threads.prepend(ThreadListItem(id, name, creator, null, null));
+    $threads.prepend(
+        ThreadListItem(id, name, creator, null, null)
+    );
     cacheThreadListElements(id);
 });
 
 //after joining and creation of element, print all messages
 socket.on('joinThread', function(messages, id, name) {
-    $chatContainer.append(ThreadWindow(id, name)); 
-    if (myThreads.length === 0) { //if this is the first window to be created, set boolean to true
-        var first = true;
-    }
+    $chatContainer.append(
+        ThreadWindow(id, name) 
+    ); 
+    
     myThreads.push(id);
-    cacheThreadElements(id, first); //put objects in $variables
+    cacheThreadElements(id); //put objects in $variables
+    if (myThreads.length === 1) { //if this is the first window to be created, set boolean to true    
+        textareaHeight = $textarea[id].outerHeight();
+        h3Height = $h3[id].outerHeight();
+    }
+    
     resizeMessages(); //resize elements by window height
-    makeCollapsible(id); //collapsing behaviour
-    makeClosable(id);
+    
     $messages[id].text('');
 	messages.forEach(function(message) {
 		$messages[id].append(Message(message.id, message.thread, message.date, message.sender, message.content));	
 	});
+    
     scrollToLastMessage(id);
-    makeTextareaSubmittable(id);
 });
 
 
@@ -70,11 +79,14 @@ function submitMessage(id) { //on form submit
 
 //on receiving a message
 socket.on('message', function(id, thread, date, sender, content) {
-    var wasAtBottom = isAtBottom($messages[thread]); //needs to be determined before appending the new message
+    var wasAtBottom = $messages[thread]._isAtBottom(); //needs to be determined before appending the new message
+  
     if (myUsername !== sender) { //if someone else sends it, notify
-        notifyOfNewMessage(thread);   
-    }   
-	$messages[thread].append(Message(id, thread, date, sender, content));
+        $thread[thread]._notifyOfNewMessage();   
+    }     
+	$messages[thread].append(
+        Message(id, thread, date, sender, content)
+    );   
     if (wasAtBottom === true) { //if you were scrolled to the bottom, scroll back to the bottom again
         scrollToLastMessage(thread, true);    
     }   
@@ -102,7 +114,7 @@ socket.on('deleteThread', function(id) {
     removeThreadListElements(id);
     if (myThreads.indexOf(id) !== -1) {
         myThreads.splice(myThreads.indexOf(id), 1);
-        removeThreadElements(id);
+        $thread[id].remove();
     }
 });
 
@@ -131,7 +143,7 @@ socket.on('showSuccess', function(header, message) {
 var $thread = [], $messagesContainer = [], $messages = [], $h3 = [], $notification = [], $textarea = [], $close = [], $lastActivity = [], $numberOfMessages = [], $threadLi = [],
     textareaHeight, h3Height;
     
-function cacheThreadElements(thread, first) {
+function cacheThreadElements(thread) {
     $thread[thread] = $('.threadContainer[data-id=' + thread + ']');
     $messagesContainer[thread] = $('.messagesContainer', $thread[thread]);
     $messages[thread] = $('.messages', $thread[thread]);
@@ -139,24 +151,6 @@ function cacheThreadElements(thread, first) {
     $notification[thread] = $('i.notification', $h3[thread]);
     $textarea[thread] = $('textarea', $thread[thread]);
     $close[thread] = $('i.close', $h3[thread]);
-    
-    if (first) {
-        textareaHeight = $textarea[thread].outerHeight();
-        h3Height = $h3[thread].outerHeight();
-    }
-}
-
-//removing object references and removing thread element on leaving
-function removeThreadElements(thread) {
-    $thread[thread].remove();
-    $thread[thread] = undefined;
-    $messagesContainer[thread] = undefined;
-    $messages[thread] = undefined;
-    $h3[thread] = undefined;
-    $notification[thread] = undefined;
-    $textarea[thread] = undefined;
-    $close[thread] = undefined;
-    lastDate[thread] = undefined;
 }
 
 function cacheThreadListElements(thread) {
@@ -174,23 +168,25 @@ function removeThreadListElements(thread) {
 //creates thread window element after joining thread
 function ThreadWindow(id, name) { //creating new element for joining
     var div = $('<div/>', { //create empty div
-        class: 'threadContainer',
-    }).attr('data-id', id),
-    html = ''; //insert empty list of messages and form into it
+            class: 'threadContainer',
+        }).attr('data-id', id),
+        html = ''; //insert empty list of messages and form into it
     html += '<h3><i class="fa fa-comment-o notification"></i><span class="threadHeaderName">' + name + '</span><i class="fa fa-times close"></i></h3>';
     html += '<div class="messagesContainer">';
     html += '<ul class="messages"></ul>';
     html += '<form class="messageForm" onSubmit="submitMessage(' + id + ');return false;">';
     html += '<textarea autocomplete="off"></textarea>';
     html += '</form></div>';
-    div.html(html); //put content inside empty div
+    div.html(html); //put content inside empty div       
+    div._makeCollapsible(); //collapsing behaviour
+    div._makeClosable();
+    div._makeSubmittable();
     return div;
 }
 
 //creates thread list item element after connection or upon creation of new thread
 function ThreadListItem(id, name, creator, lastActivity, lastSender) {
-    var li = $('<li/>', {
-    }).attr('data-id', id),
+    var li = $('<li/>').attr('data-id', id),
         _date = new Date(lastActivity),
         html = '';
     html += '<div class="threadName">' + name + '</div><div class="threadFlex">';
@@ -258,22 +254,6 @@ function scrollToLastMessage(id, animation) {
     }, duration);
 }
 
-//make thread collapsible
-function makeCollapsible(id) { 
-    $h3[id].click(function() {   //when you click tab, toggle collapsing
-        collapse(id);
-    });
-}
-
-//make thread closable
-function makeClosable(id) {
-    $close[id].click(function() { //close icon functionality
-        socket.emit('leaveThread', id);
-        myThreads.splice(myThreads.indexOf(id), 1);
-        removeThreadElements(id);
-    });
-}
-
 function makeJoinable(a, id) {
     $(a).click(function(e) { //when you click on thread, join it
         if (e.target.classList[0] !== 'fa') { //if not clicked on icon
@@ -281,43 +261,10 @@ function makeJoinable(a, id) {
             if (myThreads.indexOf(id) === -1) { //if not already joined, join
                 socket.emit('joinThread', id); 
             } else {
-                if ($thread[id].hasClass('collapsed') === true) { //else uncollapse already joined thread
-                    collapse(id);
-                }
+                $thread[id]._collapse();
             }
         }
 	});
-}
-
-//collapse given thread
-function collapse(id){
-    $thread[id].toggleClass('collapsed'); //different display of header
-    
-    if ($thread[id].hasClass('collapsed') === true) {
-        $thread[id].css('top', $messagesContainer[id].height());    
-    } else {
-        $thread[id].css('top', 0);  
-    }
-    
-    if ($thread[id].hasClass('collapsed') === false && $notification[id].hasClass('fa-comment') === true) { //if notification is up and you uncollapse it
-        scrollToLastMessage(id, true); //scroll down and remove notification
-        hideNotification(id);
-    }  
-}
-
-function makeTextareaSubmittable(id) {
-    $textarea[id].keydown(function(e) {
-        if (e.keyCode == 13) {
-            e.preventDefault();
-            if (e.shiftKey === false) {
-                $(this.form).submit()
-                return false;   
-            } else {
-                var s = $(this).val();
-                $(this).val(s + "\n");
-            }       
-        }
-    });
 }
 
 function makeThreadEditable(a, id) {
@@ -357,55 +304,11 @@ function makeThreadDeletable(a, id) {
     });
 }
 
-//notify of new message
-var notificationTimer;
-function notifyOfNewMessage(id) {
-    showNotification(id); 
-    //removing notifications 
-    if ($thread[id].hasClass('collapsed') === false) { //if not collapsed
-        if ((isAtBottom($messages[id]) === true) || ($messages[id].hasScrollBar().vertical === false)) {
-            // notification will appear briefly if at bottom or thread not big enough to have a scrollbar
-            clearTimeout(notificationTimer);
-            notificationTimer = setTimeout(function() { //after 1 second hide
-                hideNotification(id);
-            }, 1000);   
-        } else {
-            //else remove notification when you scroll down 
-            var scrollTimer;
-            $messages[id].scroll(function() { 
-                clearTimeout(scrollTimer);
-                scrollTimer = setTimeout(function() { //throttling
-                    if (isAtBottom($messages[id])) {
-                        hideNotification(id);
-                        $messages[id].off('scroll'); //remove event listener once notification is removed
-                    }
-                }, 250);
-            });
-        }
-    }
-}
-
 //checks if is scrolled to bottom in given thread
-function isAtBottom(messages) {
-    if (Math.floor(messages.scrollTop() + messages.height()) === messages[0].scrollHeight) { //if is scrolled to the bottom, continue showing new messages
-        return true;
-    } else {
-        return false;
-    }
-}
+
 
 //hides or shows notification in given thread
-function hideNotification(id) {
-    $notification[id].addClass('fa-comment-o');
-    $h3[id].removeClass('notifying');
-    $notification[id].removeClass('fa-comment');
-}
 
-function showNotification(id) {
-    $notification[id].addClass('fa-comment');
-    $h3[id].addClass('notifying');
-    $notification[id].removeClass('fa-comment-o');
-}
 
 //parses date object into string
 function showDate(date) {
@@ -419,14 +322,3 @@ function showTime(date) {
     html += (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes());
     return html;
 }
-
-//helpful method to determine whether scrolling is possible or not
-(function($) { 
-    $.fn.hasScrollBar = function() {
-        var e = this.get(0);
-        return {
-            vertical: e.scrollHeight > e.clientHeight,
-            horizontal: e.scrollWidth > e.clientWidth
-        };
-    }
-})(jQuery);
