fd031a790010f54804fa7e7f2bb5f134c5bf87f4
Václav Jančařík <vaclav.janc@gmail.com>
2016-01-13 20:10:05 +0100

    Add  favicon

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(sassMiddleware({
 
 //parser, logger and pathing
 //uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.gif')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
